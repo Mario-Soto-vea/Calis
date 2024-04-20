62896080c7cf941904af714baf47c05c675b79b5
Mario <mariosv345@gmail.com>
2024-04-20 11:50:34 -0700

    Primera version

---
+++ Metodos/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Metodos;
+
+public class Persona
+{
+    public string Nombres { get; set; }
+    public string Apellidos { get; set; }
+    public int Edad { get; set; }
+
+    public string MostrarNombre()
+    {
+        return $"{Nombres} {Apellidos}";
+    }
+    public string MostrarNombre(bool PrimeroApellidos)
+    {
+        if (PrimeroApellidos){
+            return $"{Apellidos} {Nombres}";
+        }
+        else
+            return MostrarNombre();
+    }
+    
+    public string MostrarNombre(bool PrimeroApellidos, bool Mayusculas)
+    {
+        if (PrimeroApellidos && Mayusculas)
+            return MostrarNombre(true).ToUpper();
+        else if (PrimeroApellidos && !Mayusculas)
+            return MostrarNombre(true).ToLower();
+        else if (!PrimeroApellidos && Mayusculas)
+            return MostrarNombre().ToUpper();
+        else
+            return MostrarNombre().ToLower();
+    }
+    override public string ToString(){
+        return $"Nombre: {Nombres} Apellidos: {Apellidos} Edad: {Edad}";
+    }
+}
---
+++ Metodos/Program.cs
@@ -0,0 +1,30 @@
+﻿using Metodos;
+
+var persona1 = new Persona{
+    Nombres = "Mario",
+    Apellidos = "Soto Vea",
+    Edad = 19
+};
+
+var persona2 = new Persona{
+    Nombres = "Carlos",
+    Apellidos = "Perez Barraza",
+    Edad = 67
+};
+
+
+// imprimir el nombre completo del alumno iniciando con el nombre
+Console.WriteLine(persona1.MostrarNombre());
+// imprimir el nombre completo del alumno iniciando por apellidos
+Console.WriteLine(persona1.MostrarNombre(true));
+// imprimir el nombre completo del alumno iniciando con apellidos y en minusculas
+Console.WriteLine(persona1.MostrarNombre(true,false));
+// imprimir el nombre completo del alumno iniciando con apellidos y en mayusculas
+Console.WriteLine(persona1.MostrarNombre(true,true));
+// imprimir el nombre completo del alumno iniciando con nombre y en mayusculas
+Console.WriteLine(persona1.MostrarNombre(false,true));
+// imprimir el nombre completo del alumno iniciando con nombre y en minusculas
+Console.WriteLine(persona1.MostrarNombre(false,false));
+// imprimir lo que salga del método ToString() del alumno
+Console.WriteLine(persona1.ToString());
+Console.WriteLine(persona2.ToString());
